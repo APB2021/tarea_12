260fd32cc2ec720369cdd457ea98881939ee8baf
beton <beton@i7-6700K-Betonick>
2024-11-19 11:55:40 +0100

    Primer commit con la estructura dee la tarea_12

---
+++ tarea_12/src/test/java/tarea_12/tarea_12/AppTest.java
@@ -0,0 +1,19 @@
+package tarea_12.tarea_12;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import org.junit.jupiter.api.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest {
+
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue() {
+        assertTrue(true);
+    }
+}
